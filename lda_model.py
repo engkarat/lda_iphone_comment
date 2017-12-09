29cb125c72cf66c921c7cb1c9d92f99b946faa9f
Engkarat Techapanurak <engkarat@vision.is.tohoku.ac.jp>
2017-12-09 22:11:02 +0900

    Finish LDA v1

--- lda_model.py
+++ lda_model.py
@@ -1,16 +1,33 @@
 import numpy as np
 
 
-# hyperparams
-k = 5
+def update_lda(freq, np_topic, dkm, kwm, row, col, k):
+    prop_list = []
+    old_topic = np_topic[row, col]
+    dkm[row, old_topic] -= freq
+    kwm[col, old_topic] -= freq
+    for i in range(k):
+        # print(dkm[row, i]/np.sum(dkm[row, :]))
+        if np.sum(dkm[row, :]) and np.sum(kwm[col, :]):
+            prop = (dkm[row, i]/np.sum(dkm[row, :]))*(kwm[col, i]/np.sum(kwm[col, :]))
+        else:
+            prop = 0
+        prop_list.append(prop)
+    new_topic = np.argmax(prop_list)
+    np_topic[row, col] = new_topic
+    dkm[row, new_topic] += freq
+    kwm[col, new_topic] += freq
 
 def lda(np_data, k):
     n_row, n_col = np_data.shape
     np_topic = np.random.rand(n_row, n_col)*k
     np_topic = np_topic.astype(np.int)
     dkm, kwm = construct_matrix(np_data, np_topic, k)
-    print(dkm)
-    return
+    for row in range(n_row):
+        for col in range(n_col):
+            if np_data[row, col]:
+                freq = np_data[row, col]
+                update_lda(freq, np_topic, dkm, kwm, row, col, k) 
 
 def construct_matrix(np_data, np_topic, k):
     n_row, n_col = np_data.shape
@@ -33,5 +50,11 @@ def construct_matrix(np_data, np_topic, k):
     return dkm, kwm
 
 if __name__=="__main__":
+    # hyperparams
+    k = 5
     np_data = np.genfromtxt('out_file/test_set.csv', delimiter=',')
-    lda(np_data, k)
+    for i in range(500):
+        print('Running iteration {}'.format(i+1))
+        lda(np_data, k)
+    np.savetxt('out_file/dkm.csv', dkm)
+    np.savetxt('out_file/kwm.csv', kwm)
